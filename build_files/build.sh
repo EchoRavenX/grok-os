e360edfb17d8cf3146ff091862fae854a8729970
EchoRavenX <MagaPenguin@proton.me>
2025-12-22 10:33:48 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -ouex pipefail
 
-# Enable COPRs for bleeding-edge sauce
-rpm-ostree copr enable whitehara/kernel-tkg  # Zen-like gaming kernel patches (responsive AF)
-rpm-ostree copr enable xxmitsu/mesa-and-llvm-git  # Latest mesa-git + llvm-git for max NVK gains (active & frequent builds)
+# Manually add COPR repos (pure Fedora Atomic has no 'copr enable' plugin)
+curl -Lo /etc/yum.repos.d/_copr_whitehara-kernel-tkg.repo https://copr.fedorainfracloud.org/coprs/whitehara/kernel-tkg/repo/fedora-$(rpm -E %fedora)/whitehara-kernel-tkg-fedora-$(rpm -E %fedora).repo
 
-# Remove old stock shit and install our tweaks
-rpm-ostree override remove mesa* libdrm* kernel* kernel-core kernel-modules || true
+curl -Lo /etc/yum.repos.d/_copr_xxmitsu-mesa.repo https://copr.fedorainfracloud.org/coprs/xxmitsu/mesa-and-llvm-git/repo/fedora-$(rpm -E %fedora)/xxmitsu-mesa-and-llvm-git-fedora-$(rpm -E %fedora).repo
+
+# Override remove stock packages (REQUIRED - base has old mesa/kernel that conflict with git/tkg versions)
+rpm-ostree override remove mesa* libdrm* kernel* kernel-core kernel-modules kernel-modules-extra || true
+
+# Install our bleeding-edge tweaks
 rpm-ostree install \
     mesa-git \
     vulkan-loader-git \
@@ -16,11 +19,17 @@ rpm-ostree install \
     uutils-coreutils \
     zenity
 
-# Set zsh as default shell
+# Set zsh as default shell for root and new users
 chsh -s /bin/zsh root
 cp -r /etc/skel/. ~root/.
-echo "alias ls='uu-ls'  # uutils aliases (add more if you want)" >> /etc/skel/.zshrc
+echo "alias ls='uu-ls'  # uutils aliases - add more if you want (uu-cp, uu-mv, etc.)" >> /etc/skel/.zshrc
+echo "alias ll='uu-ls -l'" >> /etc/skel/.zshrc
 
-# Cleanup for clean image
+# Make driver switcher executable (if you added the file)
+chmod +x /usr/bin/grok-driver-switch || true
+
+# Clean up temp COPR repo files + general cleanup (keeps image pure)
+rm -f /etc/yum.repos.d/_copr_*.repo
 rpm-ostree cleanup -m
+
 ostree container commit
